2a6d933db9a705eb82797a6b0f1189c81a18de25
Stephen Moore <judo2000@comcast.net>
2021-12-13 16:36:08 -0700

    Styling changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,13 +9,15 @@ $(function() {
     });
 
     
-        $("ul.navbar-nav li").click(function() {
-            var anchor = $(this).find('a').attr('href')
-            console.log(anchor);
-            $(scrollTo(anchor, 800));
-            // remove classes from all
-            $("ul.navbar-nav li").removeClass("active");
-            // add class to the one we clicked
-            $(this).addClass("active");
-         });
+    $("ul.navbar-nav li").click(function() {
+        var anchor = $(this).find('a').attr('href')
+        console.log(anchor);
+        //$(scrollTo(anchor, 2000));
+        $('html, body').animate({
+            scrollTop: $(anchor).offset().top}, 200);
+        // remove classes from all
+        $("ul.navbar-nav li").removeClass("active");
+        // add class to the one we clicked
+        $(this).addClass("active");
+        });
 });   
