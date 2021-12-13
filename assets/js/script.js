6d80a516b27caad59dc9f20c1dff4a29fc3ae71b
Stephen Moore <judo2000@comcast.net>
2021-12-13 15:07:22 -0700

    Update nav with active class and start work on my work section update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-$().ready(function() {
+$(function() {
     // target navbar elements
     let navBarSC = $('.navbar-collapse');
     let navBarNav = $('.navbar-nav');
@@ -7,4 +7,15 @@ $().ready(function() {
     navBarNav.on('click', function(e) {
         navBarSC.removeClass('show');
     });
+
+    
+        $("ul.navbar-nav li").click(function() {
+            var anchor = $(this).find('a').attr('href')
+            console.log(anchor);
+            $(scrollTo(anchor, 800));
+            // remove classes from all
+            $("ul.navbar-nav li").removeClass("active");
+            // add class to the one we clicked
+            $(this).addClass("active");
+         });
 });   
