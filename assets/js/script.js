cefeca9f30e000a1c0b09991ddcf4726e12e37ce
Stephen Moore <judo2000@comcast.net>
2021-12-16 14:23:48 -0700

    Add css to help with anchor location

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,22 +27,43 @@ $(function() {
         });
 
        // The function actually applying the offset
-function offsetAnchor() {
-    if (location.hash.length !== 0) {
-      window.scrollTo(window.scrollX, window.scrollY - 100);
+// function offsetAnchor() {
+//     if (location.hash.length !== 0) {
+//       window.scrollTo(window.scrollX, window.scrollY - 100);
+//     }
+//   }
+  
+//   // Captures click events of all <a> elements with href starting with #
+//   $(document).on('click', 'a[href^="#"]', function(event) {
+//     console.log(event.target);
+//     // Click events are captured before hashchanges. Timeout
+//     // causes offsetAnchor to be called after the page jump.
+//     window.setTimeout(function() {
+//       offsetAnchor();
+//     }, 0);
+//   });
+  
+//   // Set the offset when entering page with hash present in the url
+//   window.setTimeout(offsetAnchor, 0);
+
+document.addEventListener('DOMContentLoaded', function(event){ 
+
+    if (window.location.hash) {
+        // Start at top of page
+        window.scrollTo(0, 0);
+		
+        // Prevent default scroll to anchor by hiding the target element
+        var db_hash_elem = document.getElementById(window.location.hash.substring(1));
+        window.db_location_hash_style = db_hash_elem.style.display;
+        db_hash_elem.style.display = 'none';
+		
+        // After a short delay, display the element and scroll to it
+        jQuery(function($){
+            setTimeout(function(){
+                $(window.location.hash).css('display', window.db_location_hash_style);
+                et_pb_smooth_scroll($(window.location.hash), false, 800);
+            }, 700);
+        });		
     }
-  }
-  
-  // Captures click events of all <a> elements with href starting with #
-  $(document).on('click', 'a[href^="#"]', function(event) {
-    console.log(event.target);
-    // Click events are captured before hashchanges. Timeout
-    // causes offsetAnchor to be called after the page jump.
-    window.setTimeout(function() {
-      offsetAnchor();
-    }, 0);
-  });
-  
-  // Set the offset when entering page with hash present in the url
-  window.setTimeout(offsetAnchor, 0);
+});
 });  
