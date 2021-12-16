f04c4ceca2aad543c1564e0ec992ba0f3d86183a
Stephen Moore <judo2000@comcast.net>
2021-12-16 16:38:08 -0700

    Clean up code and add comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,20 +4,16 @@ $(function() {
     let navBarNav = $('.navbar-nav');
     let footerEl = $('#footer')
 
-    
+    // Remove class of show from mobile menu 
+    // forces the menu to close on click 
     navBarNav.on('click', function(e) {
         navBarSC.removeClass('show');
     });
-
-    footerEl.on('click', function(e) {
-        navBarSC.removeClass('show');
-    })
-
     
+    // On Click function to add active style to 
+    // clicked nav item and remove from other nav items
     $("ul.navbar-nav li").click(function() {
         var anchor = $(this).find('a').attr('href')
-        console.log(anchor);
-        //$(scrollTo(anchor, 2000));
         $('html, body').animate({
             scrollTop: $(anchor).offset().top}, 200);
         // remove classes from all
@@ -25,45 +21,4 @@ $(function() {
         // add class to the one we clicked
         $(this).addClass("active");
         });
-
-       // The function actually applying the offset
-// function offsetAnchor() {
-//     if (location.hash.length !== 0) {
-//       window.scrollTo(window.scrollX, window.scrollY - 100);
-//     }
-//   }
-  
-//   // Captures click events of all <a> elements with href starting with #
-//   $(document).on('click', 'a[href^="#"]', function(event) {
-//     console.log(event.target);
-//     // Click events are captured before hashchanges. Timeout
-//     // causes offsetAnchor to be called after the page jump.
-//     window.setTimeout(function() {
-//       offsetAnchor();
-//     }, 0);
-//   });
-  
-//   // Set the offset when entering page with hash present in the url
-//   window.setTimeout(offsetAnchor, 0);
-
-document.addEventListener('DOMContentLoaded', function(event){ 
-
-    if (window.location.hash) {
-        // Start at top of page
-        window.scrollTo(0, 0);
-		
-        // Prevent default scroll to anchor by hiding the target element
-        var db_hash_elem = document.getElementById(window.location.hash.substring(1));
-        window.db_location_hash_style = db_hash_elem.style.display;
-        db_hash_elem.style.display = 'none';
-		
-        // After a short delay, display the element and scroll to it
-        jQuery(function($){
-            setTimeout(function(){
-                $(window.location.hash).css('display', window.db_location_hash_style);
-                et_pb_smooth_scroll($(window.location.hash), false, 800);
-            }, 700);
-        });		
-    }
-});
 });  
